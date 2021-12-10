8194e50dde7458162c3ec4a89309978f955925da
mox <2422548145@qq.com>
2021-12-10 17:02:30 +0800

    first commit by hot-fix

--- src/main/java/com/mox/ssm/service/impl/AccountServiceImpl.java
+++ src/main/java/com/mox/ssm/service/impl/AccountServiceImpl.java
@@ -19,7 +19,7 @@ public class AccountServiceImpl implements AccountService {
     //使用spring来管理mybatis的工厂创建，直接注入使用（在spring的配置文件配置了sql创建）
     @Autowired
     private AccountMapper accountMapper;
-
+    //查看一个用户的信息
     @Override
     public void save(Account account) {
         accountMapper.save(account);
@@ -27,7 +27,6 @@ public class AccountServiceImpl implements AccountService {
     //查找所有 的用户
     @Override
     public List<Account> findAll() {
-
         return accountMapper.findAll();
     }
 }
